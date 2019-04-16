40b0950aae2cabde368a45e7a0bfff01a04d6478
Vivek Dalal <dalal.vi@husky.neu.edu>
2019-04-16 02:00:14 -0400

    Login with animations

--- webapp/ui/ng-Shopping-Website/src/app/components/login/login.component.ts
+++ webapp/ui/ng-Shopping-Website/src/app/components/login/login.component.ts
@@ -5,66 +5,45 @@ import {Router} from '@angular/router';
 import {AlertService} from '../../services/alert.service';
 
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  email: string;
+  password: string;
 
-  loginForm: FormGroup;
-  submitted = false;
-  loading = false;
-  returnUrl: string;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private authenticationService: AuthenticationService,
-    private router: Router,
-    private alertService: AlertService
-  ) {
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router,
+              private alertService: AlertService){
     // redirect to home if already logged in
     if (this.authenticationService.currentUserValue) {
       this.router.navigate(['/products']);
     }
   }
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
+  ngAfterViewInit() {
+    (window as any).initialize();
   }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.loginForm.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    const user = this.authenticationService.login(this.f.username.value, this.f.password.value)
+  login() {
+    console.log(`email: ${this.email} password: ${this.password}`);
+    // alert(`Email: ${this.email} Password: ${this.password}`);
+    const user = this.authenticationService.login(this.email, this.password)
       .subscribe(
         response => {
-          sessionStorage.setItem('username', this.f.username.value);
+          sessionStorage.setItem('username', this.email);
           this.router.navigate(['/products']);
         },
         error => {
           if (error.status === 400) {
             this.alertService.error('Invalid login credentials. Please enter both username and password');
-            this.loading = false;
           } else if (error.status === 401) {
             this.alertService.error('Incorrect username/password');
-            this.loading = false;
           } else {
             this.alertService.error('Something went wrong. Please try again.');
-            this.loading = false;
           }
 
         });
@@ -72,5 +51,4 @@ export class LoginComponent implements OnInit {
     return;
 
   }
-
 }
